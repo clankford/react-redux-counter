baf45aba4cdcbf0445b310778a7033263351a7e0
Chris Lankford <christopherlankford@gmail.com>
2016-08-10 18:25:17 -0700

    Added decrement action to redeucer

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function reducer(state, action) {
     if (action.type === 'INCREMENT') {
         return state + 1;
+    } else if (action.type === 'DECREMENT') {
+        return state - 1;
     } else {
         return state;
     }
@@ -10,9 +12,15 @@ const incrementAction = { type: 'INCREMENT' };
 
 console.log(reducer(0, incrementAction));
 console.log(reducer(1, incrementAction));
-console.log(reducer(5, incrementAction));
+console.log(reducer(9999, incrementAction));
 
 const unknownAction = { type: 'FAKEACTION' };
 
 console.log(reducer(5, unknownAction));
 console.log(reducer(8, unknownAction));
+
+const decrementAction = { type: 'DECREMENT' };
+
+console.log(reducer(10, decrementAction));
+console.log(reducer(10000, decrementAction));
+console.log(reducer(0, decrementAction));
