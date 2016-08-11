0fb44637ddf2c679fb7c702e7a08d7939fe8767d
Chris Lankford <christopherlankford@gmail.com>
2016-08-10 18:19:08 -0700

    Super basic reducer tests.

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ function reducer(state, action) {
         return state;
     }
 }
+
+const incrementAction = { type: 'INCREMENT' };
+
+console.log(reducer(0, incrementAction));
+console.log(reducer(1, incrementAction));
+console.log(reducer(5, incrementAction));
+
+const unknownAction = { type: 'FAKEACTION' };
+
+console.log(reducer(5, unknownAction));
+console.log(reducer(8, unknownAction));
