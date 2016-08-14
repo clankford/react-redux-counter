7fb17368a2e5f44b0c04788d1e1bbe32512ed58b
Chris Lankford <christopherlankford@gmail.com>
2016-08-14 09:45:59 -0700

    Wrote a new reducer

--- public/app.js
+++ public/app.js
@@ -12,3 +12,13 @@ function createStore(reducer, initialState) {
         dispatch,
     };
 }
+
+function reducer(state, action) {
+    if (action.type === 'ADD_MESSAGE') {
+        return {
+            messages: state.messages.concat(action.message),
+        };
+    } else {
+        return state;
+    }
+}
