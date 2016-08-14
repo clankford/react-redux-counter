afafbe92833e863fe20183ed11c116c83d45289d
Chris Lankford <christopherlankford@gmail.com>
2016-08-14 09:53:31 -0700

    Added basic tests

--- public/app.js
+++ public/app.js
@@ -22,3 +22,28 @@ function reducer(state, action) {
         return state;
     }
 }
+
+const initialState = { messages: [] };
+
+const store = createStore(reducer, initialState);
+
+const addMessageAction1 = {
+    type: 'ADD_MESSAGE',
+    message: 'Hello world!',
+};
+
+store.dispatch(addMessageAction1);
+const stateV1 = store.getState();
+
+const addMessageAction2 = {
+    type: 'ADD_MESSAGE',
+    message: 'Goodbye world!',
+};
+
+store.dispatch(addMessageAction2);
+const stateV2 = store.getState();
+
+console.log('State v1: ');
+console.log(stateV1);
+console.log('State v2: ');
+console.log(stateV2);
