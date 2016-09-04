4836ac00aeefb4840b3d19323dd558299f78aa17
Chris Lankford <christopherlankford@gmail.com>
2016-09-04 10:55:52 -0700

    Broke the reducer into two seperate reducers managing the two top level properties of the state object

--- public/app.js
+++ public/app.js
@@ -1,37 +1,49 @@
 function reducer(state, action) {
+  return {
+    activeThreadId: activeThreadIdReducer(state.activeThreadId, action),
+    threads: threadsReducer(state.threads, action),
+  }
+}
+
+function activeThreadIdReducer(state, action) {
+  if (action.type === 'OPEN_THREAD') {
+    return action.id;
+  } else {
+    return state;
+  }
+}
+
+function threadsReducer(state, action) {
     if (action.type === 'ADD_MESSAGE') {
         const newMessage = {
             text: action.text,
             timestamp: Date.now(),
             id: uuid.v4(),
         };
-        const threadIndex = state.threads.findIndex(
+        const threadIndex = state.findIndex(
             (t) => t.id === action.threadId
         );
-        const oldThread = state.threads[threadIndex];
+        const oldThread = state[threadIndex];
         const newThread =
             {
                 ...oldThread,
                 messages: oldThread.messages.concat(newMessage),
             }
-        return {
-            ...state,
-            threads: [
-                ...state.threads.slice(0, threadIndex),
+        return [
+                ...state.slice(0, threadIndex),
                 newThread,
-                 ...state.threads.slice(
-                     threadIndex + 1, state.threads.length
+                 ...state.slice(
+                     threadIndex + 1, state.length
                  ),
             ]
-        }
 
     } else if (action.type === 'DELETE_MESSAGE') {
-       const threadIndex = state.threads.findIndex(
+       const threadIndex = state.findIndex(
            (t) => t.messages.find((m) => (
                m.id === action.id
            ))
        );
-       const oldThread = state.threads[threadIndex];
+       const oldThread = state[threadIndex];
        const messageIndex = oldThread.messages.findIndex(
            (m) => m.id === action.id
        );
@@ -47,23 +59,15 @@ function reducer(state, action) {
                 messages: messages,
             }
 
-       return {
-           ...state,
-           threads: [
-                ...state.threads.slice(0, threadIndex),
+       return [
+                ...state.slice(0, threadIndex),
                 newThread,
-                ...state.threads.slice(
-                    threadIndex + 1, state.threads.length
+                ...state.slice(
+                    threadIndex + 1, state.length
                 ),
            ]
-       }
-    } else if (action.type === 'OPEN_THREAD') {
-        return Object.assign({}, state, {
-            activeThreadId: action.id,
-        });
-
     } else {
-        return state;
+      return state;
     }
 }
 
