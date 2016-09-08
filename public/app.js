5dd970c304b63d87a19279040fcac9b6fcb4ba69
Chris Lankford <christopherlankford@gmail.com>
2016-09-08 14:43:00 -0700

    Reintroduced deleting messages to the new messagesReducer

--- public/app.js
+++ public/app.js
@@ -55,15 +55,30 @@ function threadsReducer(state, action) {
 }
 
 function messagesReducer(state, action) {
-    if (action.type === 'ADD_MESSAGE') {
-        const newMessage = {
-            text: action.text,
-            timestamp: Date.now(),
-            id: uuid.v4(),
-        };
-        return state.concat(newMessage);
-    } else {
-        return state;
+    switch (action.type) {
+        switch (action.type) {
+            case 'ADD_MESSAGE': {
+                const newMessage = {
+                    text: action.text,
+                    timestamp: Date.now(),
+                    id: uuid.v4(),
+                };
+                return state.concat(newMessage);
+            }
+            case 'DELETE_MESSAGE': {
+                const messageIndex = state.findIndex((m) => m.id === action.id);
+                return [
+                    ...state.slice(0, messageIndex),
+                    ...state.slice(
+                        messageIndex + 1, state.length
+                    ),
+                ];
+            }
+            default: {
+                return state;
+            }
+        }
+    }
     }
 }
 
