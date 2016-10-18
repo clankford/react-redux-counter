01107b39c115f72b81e7d85090ad6db700a3f708
Chris Lankford <christopherlankford@gmail.com>
2016-10-17 18:57:15 -0700

    Refactored to use connect to generate the container component

--- public/components/ThreadTabs.js
+++ public/components/ThreadTabs.js
@@ -1,36 +1,71 @@
 import React from 'react';
-// Redux store
+// Redux
 import store from '../store';
+import {connect} from 'react-redux';
 // Components
 import Tabs from './Tabs';
 
-const ThreadTabs = React.createClass({
-    componentDidMount: function() {
-        store.subscribe(() => this.forceUpdate());
-    },
+// const ThreadTabs = React.createClass({
+//     componentDidMount: function() {
+//         store.subscribe(() => this.forceUpdate());
+//     },
+//
+//     render: function() {
+//         const state = store.getState();
+//         const tabs = state.threads.map(t => (
+//             {
+//                 title: t.title,
+//                 active: t.id === state.activeThreadId,
+//                 id: t.id,
+//             }
+//         ));
+//
+//         return (
+//             <Tabs
+//                 tabs={tabs}
+//                 onClick={(id) => (
+//                     store.dispatch({
+//                         type: 'OPEN_THREAD',
+//                         id: id,
+//                     })
+//                 )}
+//             />
+//         );
+//     },
+// });
 
-    render: function() {
-        const state = store.getState();
-        const tabs = state.threads.map(t => (
-            {
-                title: t.title,
-                active: t.id === state.activeThreadId,
-                id: t.id,
-            }
-        ));
 
-        return (
-            <Tabs
-                tabs={tabs}
-                onClick={(id) => (
-                    store.dispatch({
-                        type: 'OPEN_THREAD',
-                        id: id,
-                    })
-                )}
-            />
-        );
-    },
-});
+
+const mapStateToTabsProps = (state) => {
+    const tabs = state.threads.map(t => (
+        {
+            title: t.title,
+            active: t.id === state.activeThreadId,
+            id: t.id,
+        }
+    ));
+
+    return {
+        // ES6 shorthand (prop name = variable name).
+        tabs,
+    };
+};
+
+const mapDispatchToTabProps = (dispatch) => (
+    {
+        onClick: (id) => (
+            dispatch({
+                type: 'OPEN_THREAD',
+                id: id,
+            })
+        ),
+    }
+);
+
+const ThreadTabs = connect(
+    mapStateToTabsProps,
+    mapDispatchToTabProps
+)(Tabs);
+
 
 export default ThreadTabs;
